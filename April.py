251666adc1e705ef4e2a71b6d977e1592dd37547
Vincent aka Wiiz <47423231+Wiiz971@users.noreply.github.com>
2019-05-01 17:09:30 -0400

    Update April.py

--- April.py
+++ April.py
@@ -8,18 +8,14 @@ Created on Tue Apr  2 21:47:30 2019
 import random
 import numpy as np
 
-""" 
-Consigne : partir une liste de nombre L, on souhaite retourner une liste de sous liste ayant exactement le nombre délement indiqué 
-par chaque élément de la liste L. Il faut que chaque sous listes aie des éléments uniques et non presents dans les autres sous listes.
-Ces nombres sont definis aléatoirement
-"""
+# a partir une liste de nombre, on souhaite retourner une liste correspondant a des nombres aléatoies 
 def yannick(L):
     M=[]
     for i in L:
         N=[]
         if i==L[0]:
             for h in range(i):
-                q = random.randint(0,99)
+                q = random.randint(0,14)
                 if q not in N:
                     N.append(q)
             M=M+[N]
@@ -27,7 +23,7 @@ def yannick(L):
         for k in range(i):
             x=0
             while x==0:
-                q = random.randint(0,99)
+                q = random.randint(0,14)
                 for b in range(len(M)):
                     if q in N or q in M[b]:
                         x=0
@@ -39,9 +35,6 @@ def yannick(L):
         M=M+[N]
     return M
 
-"""
-Consigne : On crée une matrice de n lignes et m colonnes grâce au module numpy. Les éléments de cette matrice sont définis aléatoirement.
-"""
 def yakari(m,n): # m lignes et n colonnes
     M=[[random.randint(0,99) for i in range(n)]for j in range(m)]
     Somme=[]
@@ -59,7 +52,7 @@ def yakari(m,n): # m lignes et n colonnes
     for i in range(m):
         L1=' la somme de la %s e ligne fait :'%(i+1)
         L+=L1+' %s'%Somme[i]+','
-    return np.array(M),L,'le plus petit nombre est :',mini,'le plus grand nombre est :',maxi
+    return np.array(M),L,'le plus petit nombre est : %s'%mini,'le plus grand nombre est : %s'%maxi
  
 #On veut une fonction qui genere une pyramide d'* a partir d'une nombre d'etage donné      
 """    
@@ -83,10 +76,7 @@ def pyramide(n):
         i=S
         S=[]
     return L1
-"""
-Consigne : a partir d'une liste L, on revoie une autre liste M qui pour tout n au rang i de M correspond a la somme 
-des i-1 ème éléments de L.
-"""
+
 def add(L):
     new=[]
     for i in L:
@@ -97,19 +87,15 @@ def add(L):
         new.append(a)
     return new
 
-"""
-Consigne  : Repérer si une année est bissextile.
-"""
+
+#Faire une fonction qui renvoie la date de ton anniversaire a partir de l'agument l'année        
 def bissec(annee):
     if annee%4==0 and annee%100!=0 or annee%400==0:
         return 1
     else:
         return -1
     
-""" 
-Consigne : on demande a l'utilisateur d'entrer sa date de naissance et l'année pour laquelle il souhaite savoir le jour où son 
-anniverssaire tombera . Il ne faut utiliser le module datetime qui pour connaitre le jour de la date de naissance.
-"""
+dref = "lundi 08 Avril 2019"
 import datetime
 def yanis():
     semaine = ['Lundi' 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi','Samedi', 'Dimanche']
@@ -131,12 +117,8 @@ def yanis():
         else:
             a+=1
     b = (anniv+a)%7
-    return( 'Votre anniversaire tombera le %s %s %s %s'%(semaine[b],birthday[:-8],mois[int(birthday[-7:-5])-1], annee ))    
-
-""" 
-Consigne : on demande a l'utilisateur d'entrer sa date de naissance et l'âge pour lequel il souhaite savoir le jour où son 
-anniverssaire tombera . Il ne faut utiliser le module datetime qui pour connaitre le jour de la date de naissance.
-"""
+    return( 'Votre anniversaire tombera le %s %s %s %s'%(semaine[b],birthday[:-8],mois[int(birthday[-7:-5])-1], annee ))      
+    
 def yanis_bis():
     semaine = ['Lundi' 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi','Samedi', 'Dimanche']
     mois = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', ' Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
@@ -153,16 +135,10 @@ def yanis_bis():
             a+=1
     b = (anniv+a)%7
     return( 'Votre anniversaire tombera le %s %s %s %s'%(semaine[b],birthday[:-8],mois[int(birthday[-7:-5])-1], Ydaten ))      
-
-""" 
-Consigne : On veut connaitre la distance entre deux villes ainsi que le temps que l'on va mettre en velo, en voiture, (et a pied) pour 
-parcourir cette distance. Le resultat doit être indiqué en heure/min/sec. Une requête GET sera recommandée.
-"""
-
+    
 from bs4 import BeautifulSoup
 import urllib3
 import re
-    
     
 def distance_ville():
     def divEuclid(a,b):
@@ -205,14 +181,18 @@ def distance_ville():
     #pied = ' '.join([i for i in PO[2]])        
     return 'Il y a %s entre ces deux villes, temps : velo (16km/h) = %s , voiture(110 km/h) = %s '%(L, velo, voiture)#, a pied = %s , pied)
 
-""" 
-Consigne : On veut connaitre la valeur moyenne des éléments d'une liste.
-"""
-
+""" calcul de moyenne"""
 def valeur_moyenne(mesures):
     somme = (mesures[0]+mesures[-1])/2
     for i in range (1,len(mesures)-1):
         somme+=mesures[i]
     return somme/(len(mesures)-1)
-        
+
+""" 
+créer une liste de nombres entiers aléatoires unique avec python
+"""
+def liste(a,b,n):
+    return random.sample(range(a,b),n)
+    
+
     
